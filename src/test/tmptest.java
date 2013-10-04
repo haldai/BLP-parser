a3daec8766ab2a8f9b515af5259a679fc1645060
haldai <dai.wzero@gmail.com>
2013-10-04 13:24:20 +0800

    win environment

--- src/test/tmptest.java
+++ src/test/tmptest.java
@@ -6,8 +6,8 @@ public class tmptest {
 
 
 	public static void main(String[] args) {
-		Document doc = new Document("/home/daiwz/Myprojects/QAparser/data/questions/questions.pred", 
-				"/home/daiwz/Myprojects/QAparser/data/questions/questions.dep", true);
+		Document doc = new Document("data/questions/questions.pred", 
+				"data/questions/questions.dep", true);
 	}
 
 }
